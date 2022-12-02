0b9c86efe2cba1a4a580713a6d0f6f69e5b3c75c
jilink <jimmyfac@gmail.com>
2022-12-02 14:52:05 +0100

    correction groupe 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [pokeList, setPokeList] = useState([]);
+  const [pokeListFiltered, setPokeListFiltered] = useState([]);
+
+  async function getData() {
+    const res = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=100");
+    console.log("res", res.data);
+    setPokeList(res.data.results);
+    setPokeListFiltered(res.data.results);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  function handleChange(e) {
+    if (!e.target.value) {
+      setPokeListFiltered(pokeList);
+      return;
+    }
+
+    setPokeListFiltered(
+      pokeList.filter((pokemon) => pokemon.name.includes(e.target.value))
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={handleChange} type="text" />
+      {pokeListFiltered.map((pokemon) => {
+        return <p key={pokemon.name}>{pokemon.name}</p>;
+      })}
     </div>
   );
 }
